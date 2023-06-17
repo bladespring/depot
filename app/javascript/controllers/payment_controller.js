efb502dbf7126959f3e81cb1fe3f22693f481161
Blessed Sibanda <bladespring@gmail.com>
2023-06-17 16:55:34 +0200

    Implement tests for javascript functionality

--- app/javascript/controllers/payment_controller.js
+++ app/javascript/controllers/payment_controller.js
@@ -1,19 +1,18 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="payment"
 export default class extends Controller {
-  static targets = ['selection', 'additionalFields']
+  static targets = ["selection", "additionalFields"];
 
   initialize() {
-    this.showAdditionalFields()
+    this.showAdditionalFields();
   }
-  connect() {
-  }
+  connect() {}
 
   showAdditionalFields() {
-    let selection = this.selectionTarget.value
+    let selection = this.selectionTarget.value;
     for (let fields of this.additionalFieldsTargets) {
-      fields.disable = fields.hidden = (fields.dataset.type != selection)
+      fields.disable = fields.hidden = fields.dataset.type != selection;
     }
   }
 }
