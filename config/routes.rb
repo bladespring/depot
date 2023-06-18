2ad0a7ad7a442c0c0878136190a6876d71af10cc
Blessed Sibanda <bladespring@gmail.com>
2023-06-18 12:43:37 +0200

    Add Internationalization

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 Rails.application.routes.draw do
-  get 'admin' => 'admin#index'
+  get "admin" => "admin#index"
 
   controller :sessions do
-    get 'login' => :new
-    post 'login' => :create
-    delete 'logout' => :destroy
+    get "login" => :new
+    post "login" => :create
+    delete "logout" => :destroy
   end
+
   resources :users
   resources :orders
   resources :line_items
   resources :carts
   resources :products
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "store#index", as: "store_index"
+  scope "(:locale)" do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root "store#index", as: "store_index", via: :all
+  end
 end
