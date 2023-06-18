1459a9eb942f53600c4cba889bb98f3b60ffed41
Blessed Sibanda <bladespring@gmail.com>
2023-06-18 16:39:24 +0200

    Support request tests

--- test/controllers/support_requests_controller_test.rb
+++ test/controllers/support_requests_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class SupportRequestsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get support_requests_index_url
+    get support_requests_url
     assert_response :success
   end
 end
--- test/mailers/support_request_mailer_test.rb
+++ test/mailers/support_request_mailer_test.rb
@@ -2,11 +2,11 @@ require "test_helper"
 
 class SupportRequestMailerTest < ActionMailer::TestCase
   test "respond" do
-    mail = SupportRequestMailer.respond
-    assert_equal "Respond", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    mail = SupportRequestMailer.respond(support_requests(:one))
+    assert_equal "Re: #{support_requests(:one).subject}", mail.subject
+    assert_equal [support_requests(:one).subject], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match support_requests(:one).response.to_plain_text, mail.body.encoded
   end
 
 end
